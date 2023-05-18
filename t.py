727f689d46b05e70bada52af9102c5a864d1f4d5
ikhana <inaa.eth@gmail.com>
2023-05-18 11:14:45 +0500

    final 8 hours launch

--- t.py
+++ t.py
@@ -1,79 +1,64 @@
 import time
-import re
-import openai
-import sys
-import os
-from dotenv import load_dotenv
 import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
+from dotenv import load_dotenv
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
-from tweet_analysis import analyze_and_reply
-from tweet_actions import like_tweet, retweet, reply_to_tweet
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
 from twitter_login import login_to_twitter
 from twitter_search import twitter_search
+from tweet_analysis import analyze_and_reply
+from tweet_actions import like_tweet, retweet, reply_to_tweet
+import sys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-
+# Load env variables
 load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
-
+openai_api_key = os.getenv("OPEN_AIAPI")
 your_email_or_username = os.getenv("TWITTER_USERNAME")
 your_password = os.getenv("TWITTER_PASSWORD")
 
+# Setup webdriver
 service = Service("C:\Derivers\chromedriver_win32/chromedriver")
-
 driver = webdriver.Chrome(service=service)
 driver.get("https://twitter.com")
 
-wait = WebDriverWait(driver,30)
-
+# Login to twitter
+wait = WebDriverWait(driver, 30)
 try:
-    login_to_twitter(driver, wait)
-    # ...
-finally:
+    login_to_twitter(driver, wait, your_email_or_username, your_password)
+except:
     driver.quit()
-
-# Wait for a random period between 120 and 180 seconds, and scroll up and down
-time.sleep(random.uniform(10, 15))
-scroll_height = driver.execute_script("return document.body.scrollHeight")
-driver.execute_script(f"window.scrollTo(0, {scroll_height // 2});")
-time.sleep(2)
-driver.execute_script("window.scrollTo(0, 0);")
-
-
-
+    sys.exit("Failed to login")
 
 # Search terms
-
 search_terms = ["NFT", "Crypto", "Bitcoin", "Ethereum", "Blockchain", "DeFi", "AI", "ML", "Data Science", "Big Data", "Cloud Computing", "Cybersecurity", "IoT", "AR", "VR", "3D Printing", "Quantum Computing", "Robotics", "Drones", "Autonomous Vehicles", "Wireless Technology", "Edge Computing", "Nanotechnology", "Biotechnology", "Green Energy", "Space Technology", "Smart Cities", "Digital Marketing", "FinTech", "HealthTech", "AgriTech", "Gaming"]
 
-tweet_text = twitter_search(driver, search_terms)
+# Calculate end time as current time + 8 hours
+end_time = time.time() + 8*60*60
 
+# Keep running until 8 hours have passed
+while time.time() < end_time:
+    # Loop to run the process for 4 iterations with 15 minutes interval
+    for _ in range(4):
+        # Sleep for a random duration in the current 15-minute interval
+        time.sleep(random.uniform(_ * 15 * 60, (_ + 1) * 15 * 60))
 
+        # Pick a topic, search, like, retweet, and comment on a post
+        tweet_text = twitter_search(driver, search_terms, wait)
+        if tweet_text is not None:
+            reply = analyze_and_reply(tweet_text, openai_api_key)
+            like_tweet(driver, wait, tweet_text)
+            retweet(driver, wait)
+            reply_to_tweet(driver, wait, tweet_text, reply)
 
-# ... (continue with GPT-3 sentiment analysis and replying to the tweet)
+        # Go back to the home page
+        driver.get("https://twitter.com")
+    
+    # Sleep for a random duration in the range 60-70 minutes before the next hour starts
+    # Sleep for a random duration in the range 15-20 minutes before the next hour starts
+    time.sleep(random.uniform(15*60, 20*60))
 
-reply = analyze_and_reply(tweet_text, openai_api_key)
-
-
-
-#....................................
-
-# Now you can use these instances with your functions
-like_tweet(driver, wait, tweet_text)
-retweet(driver, wait)
-reply_to_tweet(driver, wait, tweet_text, reply)
-
-
-
-
-
-
-driver.quit()
--- twitter_search.py
+++ twitter_search.py
@@ -5,40 +5,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def twitter_search(driver, search_terms):
+def twitter_search(driver, search_terms,wait):
+    # Choose a random search term
     search_term = random.choice(search_terms)
     search_terms.remove(search_term)
 
-    search_box = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[data-testid="SearchBox_Search_Input"]')))
+    search_box = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[data-testid="SearchBox_Search_Input"]')))
     time.sleep(10)
     search_box.send_keys(search_term)
     search_box.send_keys(Keys.ENTER)
     time.sleep(10)
-
+    # Scroll to the bottom of the page
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(3)
 
-    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, '//a[contains(@href, "f=user")]/div/div/span[contains(text(), "People")]'))).click()
-    WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@aria-describedby]')))
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[role="link"][href^="/"]')))
+    # Click on the 'People' tab
+    people_tab = wait.until(EC.visibility_of_element_located((By.XPATH, '//a[contains(@href, "f=user")]/div/div/span[contains(text(), "People")]')))
+    people_tab.click()
+    wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@aria-describedby]')))
+    # Get a list of all the profile links
+    # Get a list of all the elements with 'aria-describedby' attribute
+    follow_buttons = driver.find_elements(By.XPATH, '//div[@aria-describedby]')
+    # Increase the timeout period to 20 seconds
+    wait = WebDriverWait(driver, 20)
+    # Use presence_of_element_located() instead of visibility_of_element_located()
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[role="link"][href^="/"]')))
     time.sleep(10)
 
-    profile_links = driver.find_elements(By.CSS_SELECTOR, 'a[role="link"][href^="/"]')
-    time.sleep(10)
+    # Increase the timeout period to 20 seconds
+    wait = WebDriverWait(driver, 20)
 
+# Use presence_of_element_located() instead of visibility_of_element_located()
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[role="link"][href^="/"]')))
+
+    time.sleep(10)
+    profile_links = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[role="link"][href^="/"][tabindex="-1"]')))
+    time.sleep(10)
     random_profile = random.choice(profile_links)
     random_profile.click()
     time.sleep(10)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-testid="primaryColumn"]')))
 
-    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-testid="primaryColumn"]')))
-    time.sleep(15)
 
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Tweets'))).click()
-    time.sleep(15)
+    # Wait for the profile page to load
 
+    # Wait for the page to load
+    time.sleep(5)
+    time.sleep(10)
+
+    # Click on the 'Tweets' tab
+    wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Tweets'))).click()
+
+# Wait for the tweets to load
+    time.sleep(5)
+    time.sleep(10)
+
+    # Retrieve all tweet texts
     tweet_texts = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@data-testid="tweetText"]')))
     time.sleep(10)
 
+    # If any tweets were found, select a random one, print the text and return it
     if len(tweet_texts) > 0:
         random_tweet_text = random.choice(tweet_texts)
         time.sleep(10)
