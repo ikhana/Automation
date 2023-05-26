ec073a27e9667c5191acc48bfd51a2f344069b9f
ikhana <inaa.eth@gmail.com>
2023-05-26 12:19:53 +0500

    detailed prompts

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -12,11 +12,11 @@ def analyze_and_reply(tweet_text, openai_api_key):
     print(f"The sentiment of the tweet is {sentiment}.")
 
     if sentiment == "positive":
-        prompt = f"The tweet is positive: \"{tweet_text}\". What would be a suitable response?"
+        prompt = f"The tweet is positive: \"{tweet_text}\". I want you to generate a direct reply becuase for this tweet as I will direct copying it and pasting it into content app that will automatically enter this reply to that tweet. The reply should be according to these criteri  1) It should look like more human in nature. 2) It should contain words and phrases or hashtags that will bring maximum engagment when someone see my reply. 3) It should be in appealing way that those engaging with that reply will visit my profile or follow me."
     elif sentiment == "negative":
-        prompt = f"The tweet is negative: \"{tweet_text}\". What would be a suitable response?"
+        prompt = f"The tweet is negative: \"{tweet_text}\".I want you to generate a direct reply becuase for this tweet as I will direct copying it and pasting it into content app that will automatically enter this reply to that tweet. The reply should be according to these criteri  1) It should look like more human in nature. 2) It should contain words and phrases or hashtags that will bring maximum engagment when someone see my reply. 3) It should be in appealing way that those engaging with that reply will visit my profile or follow me."
     else:
-        prompt = f"The tweet is neutral: \"{tweet_text}\". What would be a suitable response?"
+        prompt = f"The tweet is neutral: \"{tweet_text}\". I want you to generate a direct reply becuase for this tweet as I will direct copying it and pasting it into content app that will automatically enter this reply to that tweet. The reply should be according to these criteri  1) It should look like more human in nature. 2) It should contain words and phrases or hashtags that will bring maximum engagment when someone see my reply. 3) It should be in appealing way that those engaging with that reply will visit my profile or follow me."
 
     response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=60)
 
